7e34032b8ac86ce729f6349088c164836b01b395
Mohit Tilwani <mtilwani@deloitte.de>
2019-03-30 21:44:47 +0100

    use take effect and remove action from generator function param to make unit test simpler and mocking less

--- src/sagas/index.js
+++ src/sagas/index.js
@@ -1,9 +1,7 @@
-import { takeLatest } from 'redux-saga/effects';
-import { QUESTIONS, QUESTIONS_DETAILS, ADD_QUESTION } from '../constants';
 import { getQuestions, getQuestionDetails, addQuestion } from './questions';
 
 export default function* sagas() {
-  yield takeLatest(QUESTIONS.LOAD, getQuestions);
-  yield takeLatest(QUESTIONS_DETAILS.LOAD, getQuestionDetails);
-  yield takeLatest(ADD_QUESTION.LOAD, addQuestion);
+  yield getQuestions();
+  yield getQuestionDetails();
+  yield addQuestion();
 };
--- src/sagas/questions.js
+++ src/sagas/questions.js
@@ -1,8 +1,9 @@
-import { call, put } from 'redux-saga/effects';
+import { call, put, take } from 'redux-saga/effects';
 import axios from 'axios';
 import { setQuestions, setError } from '../actions/questionActions';
 import { setQuestionDetails, setQuestionDetailsError } from '../actions/questionDetails';
 import { addQuestionSuccess, addQuestionError } from '../actions/addQuestions';
+import { QUESTIONS, QUESTIONS_DETAILS, ADD_QUESTION } from '../constants';
 
 function fetchQuestionsRequest() {
   return axios({
@@ -18,8 +19,7 @@ function fetchQuestionDetailsRequest(id) {
   });
 };
 
-function addQuestionRequest({ data }) {
-  debugger;
+function addQuestionRequest(data) {
   return axios({
     method: 'post',
     url: 'http://polls.apiblueprint.org/questions',
@@ -28,42 +28,50 @@ function addQuestionRequest({ data }) {
 };
 
 export function* getQuestions() {
-  try {
-    const response = yield call(fetchQuestionsRequest);
-    const questions = response.data;
-
-    // dispatch a success action to the store with the questions
-    yield put(setQuestions(questions));
-  } catch (error) {
-    // dispatch a failure action to the store with the error
-    yield put(setError(error));
+  while (true) {
+    try {
+      yield take(QUESTIONS.LOAD);
+      const response = yield call(fetchQuestionsRequest);
+      const questions = response.data;
+  
+      // dispatch a success action to the store with the questions
+      yield put(setQuestions(questions));
+    } catch (error) {
+      // dispatch a failure action to the store with the error
+      yield put(setError(error));
+    }
   }
 };
 
-export function* getQuestionDetails(action) {
-  try {
-    const response = yield call(fetchQuestionDetailsRequest, action.id);
-    const questions = response.data;
-
-    // dispatch a success action to the store with the questions
-    yield put(setQuestionDetails(questions));
-  } catch (error) {
-    // dispatch a failure action to the store with the error
-    yield put(setQuestionDetailsError(error));
+export function* getQuestionDetails() {
+  while (true) {
+    try {
+      const { id } = yield take(QUESTIONS_DETAILS.LOAD);
+      const response = yield call(fetchQuestionDetailsRequest, id);
+      const questions = response.data;
+  
+      // dispatch a success action to the store with the questions
+      yield put(setQuestionDetails(questions));
+    } catch (error) {
+      // dispatch a failure action to the store with the error
+      yield put(setQuestionDetailsError(error));
+    }
   }
 };
 
-export function* addQuestion(action) {
-  try {
-    debugger;
-    yield call(addQuestionRequest, action.payload);
-
-    // dispatch a success action to the store with the questions
-    yield put(addQuestionSuccess())
-
-    action.payload.history.push('/');
-  } catch (error) {
-    // dispatch a failure action to the store with the error
-    yield put(addQuestionError(error));
+export function* addQuestion() {
+  while (true) {
+    try {
+      const { payload } = yield take(ADD_QUESTION.LOAD);
+      yield call(addQuestionRequest, payload.data);
+  
+      // dispatch a success action to the store with the questions
+      yield put(addQuestionSuccess())
+  
+      payload.history.push('/');
+    } catch (error) {
+      // dispatch a failure action to the store with the error
+      yield put(addQuestionError(error));
+    }
   }
 };
