9f1944e3ee1632c2408c7c12975fa0e479f9f459
Mohit Tilwani <mtilwani@deloitte.de>
2019-03-31 15:26:02 +0200

    use styled components to apply global styles

--- src/index.js
+++ src/index.js
@@ -1,15 +1,29 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
+import { createGlobalStyle } from 'styled-components';
 import App from './components/App';
 import configureStore from './createStore';
 import * as serviceWorker from './serviceWorker';
-import './index.css';
 
 const store = configureStore();
 
+const GlobalStyle = createGlobalStyle`
+  body {
+    background: -webkit-linear-gradient(top, #4357ff 0%,#4357ff 50%,#3745af 51%,#3745af 100%);
+    display: flex;
+    align-items: center;
+    justify-content: center;
+
+    a {
+      text-decoration: none;
+    }
+  }
+`;
+
 ReactDOM.render(
   <Provider store={store}>
+    <GlobalStyle />
     <App />
   </Provider>,
   document.getElementById('root')
